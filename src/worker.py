cdcca574c0b4a40363921659aa3747bbd5f9f456
expl0it <solovey.anton@gmail.com>
2021-08-24 18:47:53 +0300

    Some minor refactorings done

--- src/worker.py
+++ src/worker.py
@@ -12,8 +12,10 @@ import pickledb
 import urllib3
 from pytz import UTC
 
-import storage
-import service
+try:
+    import service
+except ImportError:
+    from src import service
 
 urllib3.disable_warnings()
 
@@ -23,6 +25,12 @@ FEED_CHUNK_SIZE = 1048576
 
 
 class Downloader:
+    def __init__(self) -> None:
+        self.workdir = os.path.dirname(os.path.realpath(__file__))
+        self.cache = pickledb.load(
+            os.path.join(self.workdir, "cache/cache.json"), auto_dump=False
+        )
+
     async def get_feed(self, feed: Dict[str, Any]) -> Optional[Dict[str, Any]]:
         """
         Download the feed specified. Just get the feed of its own format without any parsing.
@@ -40,6 +48,8 @@ class Downloader:
                 )
                 if response.status_code == 200:
                     feed_download_time = time() - time_start
+                    # TODO: parametrize the option below
+                    #
                     # In case you wanna dump feed to the disk
                     # storage.write_to_disk(f"{feed['feed_name']}.{feed['feed_type']}", response.text)
                     hash = hashlib.md5(response.content).hexdigest()
@@ -57,6 +67,22 @@ class Downloader:
                 logger.error(f"Feed `{feed['feed_name']}` can not be downloaded: {e}")
                 unsuccessfully_downloaded += 1
 
+    def is_updated(self, feed_name: str, feed_hash: str) -> bool:
+        """
+        Check if current feed hash is equal
+        to the previous feed hash then returns False,
+        otherwise method returns True
+        """
+        if self.cache.exists(feed_name):
+            if self.cache.get(feed_name) == feed_hash:
+                return False
+            else:
+                return True
+        else:
+            self.cache.set(feed_name, feed_hash)
+            self.cache.dump()
+            return True
+
     async def get_all_osint_feeds(self, feeds: List[Dict[str, Any]]) -> None:
         """
         Downloads all opensource feeds from
@@ -66,33 +92,32 @@ class Downloader:
         data = [(self.get_feed(feed)) for feed in feeds]
         results = await asyncio.gather(*data, return_exceptions=False)
 
-        self.workdir = os.path.dirname(os.path.realpath(__file__))
-        self.cache = pickledb.load(
-            os.path.join(self.workdir, "cache/cache.json"), auto_dump=False
-        )
-
         batch_results: List[Tuple] = []
         updated_feeds: List[str] = []
 
         for feed in results:
             if feed:
-                for k, v in feed.items():
-
-                    if self.cache.exists(k):
-                        if self.cache.get(k) == v:
-                            # print(f"Feed {k} has not changed since last update")
-                            batch_results.append((k, datetime.now(UTC), 0))
-                    else:
+                for feed_name, feed_hash in feed.items():
+                    if self.is_updated:
+                        updated_feeds.append(feed_name)
+                        batch_results.append((feed_name, datetime.now(UTC), 1))
                         # print(f"Feed {k} has been updated {v}")
-                        updated_feeds.append(k)
-                        self.cache.set(k, v)
-                        batch_results.append((k, datetime.now(UTC), 1))
-
-        storage.write_stats(batch_results)
-        self.cache.dump()
+                    elif self.is_updated == False:
+                        batch_results.append((feed_name, datetime.now(UTC), 0))
+                        # print(f"Feed {k} has not changed since last update")
+        # storage.write_stats(batch_results)
         logger.info(
             f"{len(updated_feeds)} feeds updated. Updated feeds: {updated_feeds}"
         )
+        return batch_results
+
+    def save_to_database(self, data: List[Tuple]):
+        try:
+            import storage
+        except ImportError:
+            from src import storage
+
+        storage.write_stats(data)
 
     def get_feeds(self, feeds: List[Dict[str, Any]]) -> None:
         """
@@ -102,7 +127,8 @@ class Downloader:
         time_start = time()
 
         try:
-            asyncio.run(self.get_all_osint_feeds(feeds))
+            results = asyncio.run(self.get_all_osint_feeds(feeds))
+            self.save_to_database(results)
         finally:
             logger.info(
                 f"Successfully downloaded {len(feeds)} feeds in {(time() - time_start):.2f} seconds"
